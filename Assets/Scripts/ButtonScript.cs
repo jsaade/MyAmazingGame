640a77ae4c1cddceee6140bae196eb815d297ffa
jsaade <jsaade@gmail.com>
2025-02-09 12:11:19 +0300

    Add script onClick on buttton in main menu

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClick()
     {
-        
+        //do something!
+        Debug.Log("button clicked");
     }
 }
